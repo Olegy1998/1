29a599e8fa5a5a0a7610f5ec451cb9454f4f04c2
Oleg Gubarev <oleg.gubarevv@mail.ru>
2018-06-19 18:42:51 +0300

    No commit

---
+++ PycharmProjects/mario/sdacha.py
@@ -0,0 +1,11 @@
+money = int(input("Введите число, с которого нужно выдать дачу: "))
+if money > 0 and money <= 99:
+for i in range(money):
+ch1 = money // 50
+ch11 = money - 50 * ch1
+ch2 = ch11 // 10
+ch22 = ch11 - 10*ch2
+ch3 = ch22 // 5
+ch33 = ch22 - 5*ch3
+ch4 = ch33 // 1
+print(ch1,ch2,ch3,ch4)
