bded1778ba725f1e55f5d54352bbf3e956fae23c
Oleg Gubarev <oleg.gubarevv@mail.ru>
2018-06-24 13:14:03 +0300

    No commit

---
+++ PycharmProjects/mario/fathers-beginer.py
@@ -0,0 +1,19 @@
+fathers = {
+    "Guido van Rossum": "Python",
+    "Thomas Kurtz": "Basic",
+    "Rasmus Lerdorf": "PHP",
+    "James Gosling": "Jawa",
+    "Yukishiro Matsomoto": "Ruby"
+    "Bjarne Stroustrup": "C++"
+}
+dictionary = input('Введите List или Name: ')
+if dictionary == 'Name':
+    Name = input('Введите имя отца-основателя')
+    for name, value in fathers.items():
+        if name == Name
+            print (Name ,'является создателем', fathers[name])
+else:
+    if dictionary == 'List'
+        name = sorted(fathers.keys())
+        for work in Name:
+            print(work, 'является создателем', fathers[work])
